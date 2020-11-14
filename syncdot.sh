031639e42dd127e895e4f5c254f7b9c14ebc4183
Brian Vess <brian.vess@gmail.com>
2020-11-14 07:02:07 -0500

    Add brew and npm backup list

--- syncdot.sh
+++ syncdot.sh
@@ -7,3 +7,14 @@ rsync -avh ~/.config/nvim/init.vim .
 rsync -avh ~/.config/nvim/coc-settings.json .
 rsync -avh ~/.config/alacritty/alacritty.yml .
 rsync -avh ~/.p10k.zsh .
+
+# Save homebrew files
+if type brew > /dev/null; then
+    brew list --formula > brewfiles
+fi
+
+# Save npm global packages
+if type npm > /dev/null; then
+    npm ls --depth=0 -g > npmfiles
+fi
+
